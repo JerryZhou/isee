e43fcc49bb27ab3b4ea890f6bce7db5dfd8ab7fe
jerry <jerryzhou@outlook.com>
2016-08-15 15:18:15 +0800

    add test for imemory

--- test/foundation/test_imemory.h
+++ test/foundation/test_imemory.h
@@ -1,2 +1,26 @@
 
 #include "simpletest.h"
+#include "foundation/imemory.h"
+
+// **********************************************************************************
+// imemory
+SP_SUIT(imemory);
+
+SP_CASE(imemory, icallocANDifree) {
+    int *i = (int*)icalloc(1, sizeof(int));
+    SP_EQUAL(*i, 0);
+    ifree(i);
+}
+
+SP_CASE(imemory, irealloc) {
+    int *i = (int*)icalloc(2, sizeof(int));
+    SP_EQUAL(i[0], 0);
+    SP_EQUAL(i[1], 0);
+    i[1] = 5;
+    SP_EQUAL(i[1], 5);
+    i = (int*)irealloc(i, sizeof(int)*3);
+    SP_EQUAL(i[0], 0);
+    SP_EQUAL(i[1], 5);
+    SP_EQUAL(i[2], 0);
+    ifree(i);
+}
