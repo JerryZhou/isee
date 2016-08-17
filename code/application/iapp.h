b9e2f3f6f64fe7574df1bb788b389d222416dee8
jerry <jerryzhou@outlook.com>
2016-08-17 10:14:35 +0800

    add example folder,  add application-lib, add graphics-lib

---
+++ code/application/iapp.h
@@ -0,0 +1,6 @@
+#ifndef _IAPP_H_
+#define _IAPP_H_
+
+#include "foundation/core/iref.h"
+
+#endif
---
+++ code/graphics/igraphics.h
@@ -0,0 +1,23 @@
+#ifndef _IGRAPHICS_H_
+#define _IGRAPHICS_H_
+
+#include "foundation/core/iref.h"
+
+typedef struct igraphicscontext {
+    irefdeclare;
+}igraphicscontext;
+
+typedef struct imodel {
+    irefdeclare;
+} imodel;
+
+typedef struct itexture {
+    irefdeclare;
+}itexture;
+
+typedef struct ishader {
+    irefdeclare;
+}ishader;
+
+
+#endif
