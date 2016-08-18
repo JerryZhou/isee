e306425e7a41ccec4b097c60313e94c2babea734
jerry <jerryzhou@outlook.com>
2016-08-18 11:59:55 +0800

    dummys fix

--- code/application/iapp.c
+++ code/application/iapp.c
@@ -1,1 +1,14 @@
 #include "application/iapp.h"
+
+iapplication *iapplicationmake() {
+    iapplication *app = irefnew(iapplication);
+    return app;
+}
+
+#include "foundation/core/imeta.define.inl"
+__imeta_all_application;
+#include "foundation/core/imeta.imp.inl"
+void iapplicationinit() {
+    __iudeclare;
+    __imeta_all_application;
+}
--- code/application/iapp.h
+++ code/application/iapp.h
@@ -2,5 +2,19 @@
 #define _IAPP_H_
 
 #include "foundation/core/iref.h"
+#include "foundation/core/imetatypes.h"
+
+void iapplicationinit();
+
+typedef struct iapplication {
+    irefdeclare;
+}iapplication;
+
+iapplication *iapplicationmake();
+
+#define __imeta_all_application \
+    __iudeclaremeta(iapplication)
+
+__imeta_all_application;
 
 #endif
