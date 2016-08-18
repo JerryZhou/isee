c67071ea51bf0d9114979fd9fd2d8ee331f761de
jerry <jerryzhou@outlook.com>
2016-08-18 12:00:44 +0800

    use the ishader

--- example/01simplewindow/main.c
+++ example/01simplewindow/main.c
@@ -42,7 +42,8 @@ int _x_ientry_window_event(struct iwindow *win, ievent *event) {
 /* cross-platorm window: draw the window */
 int _x_ientry_window_draw(struct iwindow *win) {
     static int initdraws = 0;
-    static GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+    static ishader *shader;
+    static GLuint vertex_buffer;
     static GLint mvp_location, vpos_location, vcol_location;
     float ratio;
     int width, height;
@@ -52,28 +53,22 @@ int _x_ientry_window_draw(struct iwindow *win) {
     if (initdraws == 1) {
         glfwSwapInterval(1);
         // NOTE: OpenGL error checks have been omitted for brevity
+        
+        shader = ishadercreate(vertex_shader_text, fragment_shader_text);
+        mvp_location = ishadergetuniformloc(shader, "MVP");
+        vpos_location = ishadergetattriloc(shader, "vPos");
+        vcol_location = ishadergetattriloc(shader, "vCol");
+        
         glGenBuffers(1, &vertex_buffer);
         glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-        vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-        glCompileShader(vertex_shader);
-        fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-        glCompileShader(fragment_shader);
-        program = glCreateProgram();
-        glAttachShader(program, vertex_shader);
-        glAttachShader(program, fragment_shader);
-        glLinkProgram(program);
-        mvp_location = glGetUniformLocation(program, "MVP");
-        vpos_location = glGetAttribLocation(program, "vPos");
-        vcol_location = glGetAttribLocation(program, "vCol");
         glEnableVertexAttribArray(vpos_location);
         glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
                               sizeof(float) * 5, (void*) 0);
         glEnableVertexAttribArray(vcol_location);
         glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
                               sizeof(float) * 5, (void*) (sizeof(float) * 2));
+        
     }
     
     /* frame size */
@@ -86,7 +81,7 @@ int _x_ientry_window_draw(struct iwindow *win) {
     mat4x4_rotate_Z(m, m, (float) glfwGetTime());
     mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
     mat4x4_mul(mvp, p, m);
-    glUseProgram(program);
+    ishaderuse(shader);
     glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
     glDrawArrays(GL_TRIANGLES, 0, 3);
     
