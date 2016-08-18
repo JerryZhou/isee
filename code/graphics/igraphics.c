9205061ebb2bd8e706186bd5b4fad1331650f292
jerry <jerryzhou@outlook.com>
2016-08-18 10:26:31 +0800

    graphics

--- code/graphics/igraphics.c
+++ code/graphics/igraphics.c
@@ -1,1 +1,255 @@
 #include "graphics/igraphics.h"
+
+#include "glfw/glfw3.h"
+
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    ievent event = {key, scancode, action, mods};
+    iwindow *win = icast(iwindow, glfwGetWindowUserPointer(window));
+    if (win->entry && win->entry->entryevent) {
+        win->entry->entryevent(win, &event);
+    }
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        iwindowdestroy(win);
+    }
+}
+
+void igraphicsapp_destructor(ithis x, iobj *o) {
+    igraphicsapp *app = icast(igraphicsapp, __irobj(o));
+    irelease(app->windows);
+    
+    glfwTerminate();
+}
+
+/* make a app */
+igraphicsapp *igraphicsappmake() {
+    igraphicsapp *app = irefnew(igraphicsapp);
+    app->windows = ireflistmake();
+    
+    return app;
+}
+
+/* init the app */
+int igraphicsappinit(igraphicsapp *app) {
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    return iiyes;
+}
+
+/* init the app */
+int igraphicsappinitwithwindow(igraphicsapp *app, const char* name, int w, int h,
+                               iwindowentry *entry) {
+    iwindow *win;
+    igraphicsappinit(app);
+    win = igraphicsappcreatewindow(app, name, w, h, entry);
+    irelease(win); /* release refs */
+    return iiyes;
+}
+
+/* create a window, auto attched to app */
+iwindow *igraphicsappcreatewindow(igraphicsapp *app,
+                                  const char* name, int w, int h,
+                                  iwindowentry *entry) {
+    GLFWwindow* glfxwin = NULL;
+    iwindow *win = NULL;
+    
+    /* platform window */
+    glfxwin = glfwCreateWindow(w, h, name, NULL, NULL);
+    icheckret(glfxwin, win);
+    
+    /* cross-platform window */
+    win = irefnew(iwindow);
+    win->app = iwrefmake(irefcast(win));
+    win->entry = entry;
+    win->name = istringmake(name);
+    win->width = w;
+    win->height = h;
+    win->wthis = (ithis)glfxwin;
+    
+    glfwSetKeyCallback(glfxwin, key_callback);
+    glfwSetWindowUserPointer(glfxwin, win);
+    
+    /* add to window */
+    ireflistadd(app->windows, irefcast(win));
+    
+    return win;
+}
+
+void igraphicsappasyncremovewin(igraphicsapp *app, iwindow *win) {
+    /* asyncd remove from app */
+    ireflistremove(app->asyncremoves, irefcast(win));
+}
+
+
+/* exit app */
+int igraphicsappexit(igraphicsapp *app) {
+    iflag_add(app->flag, EnumGraphicsAppFlag_Exit);
+    return app->flag;
+}
+
+/* poll events */
+void igraphicappspollevents(igraphicsapp *app) {
+    glfwPollEvents();
+}
+
+/* window async remove */
+void igraphicsappprocesswindowremove(igraphicsapp *app) {
+    iwindow *win = NULL;
+    
+    /* remove destroyed windows */
+    irefjoint *joint = ireflistfirst(app->asyncremoves);
+    while (joint) {
+        ireflistremove(app->windows, joint->value);
+        joint = joint->next;
+    }
+    
+    /* remove closed windows  */
+    joint = ireflistfirst(app->windows);
+    while (joint) {
+        win = icast(iwindow, joint->value);
+        if (iwindowisclose(win)) {
+            joint = ireflistremovejoint(app->windows, joint);
+        } else {
+            joint = joint->next;
+        }
+    }
+}
+
+/* start the event loop */
+int igraphicsapploop(igraphicsapp *app) {
+    irefjoint *joint;
+    iwindow *win;
+    while (igraphicsappisexit(app) == iino) {
+       
+        /* loop the windows */
+        joint = ireflistfirst(app->windows);
+        while (joint) {
+            /* cast window */
+            win = icast(iwindow, joint->value);
+           
+            /* one-frame */
+            iwindowframe(win, 0);
+            
+            /* to next */
+            joint = joint->next;
+        }
+        /* event */
+        igraphicappspollevents(app);
+        /* process window close and remove */
+        igraphicsappprocesswindowremove(app);
+    }
+    return 0;
+}
+
+/* all graphics types */
+#include "foundation/core/imeta.define.inl"
+__iallmeta_graphics;
+#include "foundation/core/imeta.imp.inl"
+int igraphicsinit() {
+    imetaconfig config;
+    __iallmeta_graphics;
+    return 0;
+}
+
+int igraphicsappisexit(igraphicsapp *app) {
+    if(iflag_is(app->flag, EnumGraphicsAppFlag_Exit)) {
+        return iiyes;
+    }
+    
+    if (ireflistlen(app->windows) == 0) {
+        return iiyes;
+    }
+    return iino;
+}
+
+/* wrap the glfx window destroy */
+static void _iwindow_glfx_destroy(iwindow *win) {
+    GLFWwindow* glfxwin = (GLFWwindow*)win->wthis;
+    icheck(glfxwin);
+    
+    glfwSetKeyCallback(glfxwin, NULL);
+    glfwSetWindowUserPointer(glfxwin, NULL);
+    glfwDestroyWindow(glfxwin);
+    win->wthis = NULL;
+}
+
+/* wrap the glfx window close */
+static void _iwindow_glfx_close(iwindow *win) {
+    GLFWwindow* glfxwin = (GLFWwindow*)win->wthis;
+    icheck(glfxwin);
+    
+    glfwSetWindowShouldClose(glfxwin, GLFW_TRUE);
+}
+
+/* wrap the glfx window close flag */
+static int _iwindow_glfx_isclose(iwindow *win) {
+    GLFWwindow* glfxwin = (GLFWwindow*)win->wthis;
+    icheckret(glfxwin, iiyes);
+    
+    return glfwWindowShouldClose(glfxwin) == GLFW_TRUE;
+}
+
+/* wrap the glfx window being current */
+static void _iwindow_glfx_current(iwindow *win) {
+    GLFWwindow* glfxwin = (GLFWwindow*)win->wthis;
+    icheck(glfxwin);
+    glfwMakeContextCurrent(glfxwin);
+}
+
+void iwindow_destructor(ithis x, iobj *o) {
+    iwindow *win = icast(iwindow, __irobj(o));
+    _iwindow_glfx_destroy(win);
+}
+
+/* close the window */
+void iwindowclose(iwindow *win) {
+    _iwindow_glfx_close(win);
+}
+
+/* if the window has closed */
+int iwindowisclose(iwindow *win) {
+    return _iwindow_glfx_isclose(win);
+}
+
+/* destroy the window */
+void iwindowdestroy(iwindow *win) {
+    igraphicsapp *app = icast(igraphicsapp, iwrefunsafestrong(win->app));
+    _iwindow_glfx_destroy(win);
+    igraphicsappasyncremovewin(app, win);
+}
+
+void iwindowframe(iwindow *win, int64_t frameticks) {
+    /* make current context */
+    _iwindow_glfx_current(win);
+    icheck(win->entry);
+    /* window update */
+    if (win->entry->entryupdate) {
+        win->entry->entryupdate(win, frameticks);
+    }
+    
+    /* window draw */
+    if (win->entry->entrydraw) {
+        win->entry->entrydraw(win);
+    }
+}
+
+/* get the frame buffer size of window */
+void iwindowframebuffersize(iwindow *win, int *w, int *h) {
+    GLFWwindow* glfxwin = (GLFWwindow*)win->wthis;
+    icheck(glfxwin);
+    glfwGetFramebufferSize(glfxwin, w, h);
+}
+
+/* swap the backend and frontend buffer */
+void iwindowswap(iwindow *win) {
+    GLFWwindow* glfxwin = (GLFWwindow*)win->wthis;
+    
+    glfwSwapBuffers(glfxwin);
+}
--- code/graphics/igraphics.h
+++ code/graphics/igraphics.h
@@ -2,6 +2,121 @@
 #define _IGRAPHICS_H_
 
 #include "foundation/core/iref.h"
+#include "foundation/core/imetatypes.h"
+#include "foundation/util/istring.h"
+#include "foundation/container/ireflist.h"
+
+/* init the graphcis-run-time */
+int igraphicsinit();
+
+/* forwrad declares */
+struct iwindow;
+struct igraphicsapp;
+
+/* basic event wrap */
+typedef struct ievent {
+    int key;
+    int scancode;
+    int action;
+    int mods;
+}ievent;
+
+/* cross-platorm window: event handler */
+typedef int (*ientry_window_event)(struct iwindow *win, ievent *event);
+/* cross-platorm window: draw the window */
+typedef int (*ientry_window_draw)(struct iwindow *win);
+/* cross-platorm window: update the window */
+typedef int (*ientry_window_update)(struct iwindow *win, int64_t ticks);
+
+/* window entrys */
+typedef struct iwindowentry {
+    ientry_window_event entryevent;
+    ientry_window_draw entrydraw;
+    ientry_window_update entryupdate;
+}iwindowentry;
+
+/* cross-platorm window */
+typedef struct iwindow {
+    irefdeclare;
+    
+    /* attached app */
+    iwref *app;
+    
+    istring name;   /* window propertys: name */
+    
+    int width;      /* window propertys: width */
+    int height;     /* window propertys: height */
+    int flag;       /* window propertys: flag */
+    
+    iwindowentry *entry;
+    
+    /* private value */
+    ithis wthis;
+}iwindow;
+
+/* close the window */
+void iwindowclose(iwindow *win);
+
+/* if the window have been closed, return iiyes or iino */
+int iwindowisclose(iwindow *win);
+
+/* destroy the window */
+void iwindowdestroy(iwindow *win);
+
+/* disptach the window frame */
+void iwindowframe(iwindow *win, int64_t frameticks);
+
+/* get the frame buffer size of window */
+void iwindowframebuffersize(iwindow *win, int *w, int *h);
+
+/* swap the backend and frontend buffer */
+void iwindowswap(iwindow *win);
+
+/* app flag */
+typedef enum EnumGraphicsAppFlag {
+    EnumGraphicsAppFlag_Exit = 1<<1,
+}EnumGraphicsAppFlag;
+
+/* the event loop */
+typedef struct igraphicsapp {
+    irefdeclare;
+    
+    ireflist *windows; /* current windows */
+    
+    /* async removed windows */
+    ireflist *asyncremoves;
+    
+    /* flag */
+    int flag;
+    
+}igraphicsapp;
+
+/* app destructor */
+void igraphicsapp_destructor(ithis x, iobj *o);
+
+/* make a app */
+igraphicsapp *igraphicsappmake();
+
+/* init the app */
+int igraphicsappinit(igraphicsapp *app);
+
+int igraphicsappinitwithwindow(igraphicsapp *app, const char* name, int w, int h,
+                               iwindowentry *entry);
+
+/* create a window, auto attched to app */
+iwindow *igraphicsappcreatewindow(igraphicsapp *app,
+                                  const char* name, int w, int h,
+                                  iwindowentry *entry);
+
+/* exit app */
+int igraphicsappexit(igraphicsapp *app);
+
+/* if the app exited */
+int igraphicsappisexit(igraphicsapp *app);
+
+/* start the event loop */
+int igraphicsapploop(igraphicsapp *app);
+
 
 typedef struct igraphicscontext {
     irefdeclare;
@@ -19,5 +134,12 @@ typedef struct ishader {
     irefdeclare;
 }ishader;
 
+#define __iallmeta_graphics \
+    __iudeclaremeta(iwindow);\
+    __iudeclaremetapart(igraphicsapp, 0, NULL, igraphicsapp_destructor)
+
+/* declare all the meta-indexs */
+__iallmeta_graphics;
+    
 
 #endif
