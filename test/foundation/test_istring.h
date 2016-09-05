93c1a8edbb676da6b1350578e02654919ece30fa
jerry <jerryzhou@outlook.com>
2016-09-06 00:17:53 +0800

    istring-test: istringfind

--- test/foundation/test_istring.h
+++ test/foundation/test_istring.h
@@ -114,6 +114,20 @@ SP_CASE(istring, istringcompare) {
     irelease(s2);
 }
 
+SP_CASE(istring, istringfind) {
+    /* 1-2-3-a-b-c- -1-2-3-  -a -b -c -d -  -1 -2 -3 -4 -  -a - */
+    /* 0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21*/
+    istring *s0 = istringmake("123abc 123 abcd 1234 a");
+    int index = istringfind(s0, "123", 3, 0);
+    SP_TRUE(index == 0);
+    index = istringfind(s0, "1234", 4, 0);
+    SP_TRUE(index == 16);
+    index = istringfind(s0, "12345", 5, 0);
+    SP_TRUE(index == kindex_invalid);
+    
+    irelease(s0);
+}
+
 SP_CASE(istring, end) {
     SP_TRUE(1);
 }
