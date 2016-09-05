1f913eba6e8a071fdcba7c32f57846d7977b5a3e
jerry <jerryzhou@outlook.com>
2016-09-05 23:47:39 +0800

    test for istring

---
+++ test/foundation/test_istring.h
@@ -0,0 +1,69 @@
+#include "foundation/util/istring.h"
+#include "simpletest.h"
+
+SP_SUIT(istring);
+
+SP_CASE(istring, kstring_zeroANDkstring_empty) {
+    SP_TRUE(istringlen(kstring_zero) == 0);
+    SP_TRUE(kstring_zero == NULL);
+    
+    SP_TRUE(istringlen(kstring_empty) == 0);
+    SP_TRUE(kstring_empty != NULL);
+}
+
+SP_CASE(istring, istringmakeANDistringmakelen) {
+    istring *s = istringmake(NULL);
+    SP_TRUE(s == kstring_zero);
+    
+    istring *s0 = istringmake("");
+    SP_TRUE(s0 == kstring_empty);
+    
+    istring *s1 = istringmake("123");
+    SP_TRUE(istringlen(s1) == 3);
+    SP_TRUE(istringbuf(s1)[0] == '1');
+    SP_TRUE(istringbuf(s1)[1] == '2');
+    SP_TRUE(istringbuf(s1)[2] == '3');
+    SP_TRUE(istringbuf(s1)[3] == 0);
+    
+    irelease(s);
+    irelease(s0);
+    irelease(s1);
+}
+
+SP_CASE(istring, istringdup) {
+    istring *s0 = istringmake("0123");
+    istring *s1 = istringdup(s0);
+    
+    SP_TRUE(s0 != s1);
+    SP_TRUE(istringcompare(s0, s1)==0);
+    
+    irelease(s0);
+    irelease(s1);
+}
+
+SP_CASE(istring, istringlen) {
+    istring *s0 = istringmake("0123");
+    
+    SP_TRUE(istringlen(s0) == 4);
+    
+    irelease(s0);
+}
+
+SP_CASE(istring, istringbuf) {
+    istring *s0 = istringmake("0123");
+    
+    SP_TRUE(istringlen(s0) == 4);
+    
+    const char* sbuf = istringbuf(s0);
+    SP_TRUE(sbuf[0] == '0');
+    SP_TRUE(sbuf[1] == '1');
+    SP_TRUE(sbuf[2] == '2');
+    SP_TRUE(sbuf[3] == '3');
+    SP_TRUE(sbuf[4] == 0);
+    
+    irelease(s0);
+}
+
+SP_CASE(istring, end) {
+    SP_TRUE(1);
+}
