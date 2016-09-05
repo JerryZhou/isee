eedc1964b640ee9b7da3c1e35cc5c7666550e7fc
jerry <jerryzhou@outlook.com>
2016-09-06 00:02:43 +0800

    add test - istring: istringlaw, istringformat

--- test/foundation/test_istring.h
+++ test/foundation/test_istring.h
@@ -64,6 +64,36 @@ SP_CASE(istring, istringbuf) {
     irelease(s0);
 }
 
+SP_CASE(istring, istringlaw) {
+    istring *s0 = istringmake("0123");
+    
+    SP_TRUE(istringlen(s0) == 4);
+    
+    const char* sbuf = istringbuf(s0);
+    SP_TRUE(sbuf[0] == '0');
+    SP_TRUE(sbuf[1] == '1');
+    SP_TRUE(sbuf[2] == '2');
+    SP_TRUE(sbuf[3] == '3');
+    SP_TRUE(sbuf[4] == 0);
+    
+    SP_TRUE(s0->array->entry == iarrayentryget(EnumArrayEntryType_Char));
+    SP_TRUE(iflag_is(s0->flag, EnumSliceFlag_String));
+    
+    irelease(s0);
+}
+
+SP_CASE(istring, istringformat) {
+    istring * s = istringmake("abc");
+    istring * s0 = istringformat("i:%i,I:%I,v:%v,V:%V,u:%u,U:%U,s:%s,f:%f,F:%F", 1, int64_t(1), s, s, (unsigned int)(1), (uint64_t)(1), "abc", 1.0, 1.0);
+    istring * s1 = istringmake("i:1,I:1,v:abc,V:abc,u:1,U:1,s:abc,f:1.0000,F:1.0000");
+    
+    SP_TRUE(istringcompare(s0, s1) == 0);
+    
+    irelease(s);
+    irelease(s0);
+    irelease(s1);
+}
+
 SP_CASE(istring, end) {
     SP_TRUE(1);
 }
