c8a3a2c14c2a508b638593670303239d55bd4534
jerry <jerryzhou@outlook.com>
2016-10-07 23:29:41 +0800

    add basic quat test

--- test/foundation/test_iquat.h
+++ test/foundation/test_iquat.h
@@ -28,6 +28,53 @@ SP_CASE(iquat, iquatln) {
     SP_TRUE(1);
 }
 
+SP_CASE(iquat, iquatinvert) {
+    iquat quat;
+    __iscaler_fill_4(&quat, 1, 2, 3, 4);
+    
+    iquat quatinvert;
+    __iscaler_fill_4(&quatinvert, -1, -2, -3, -4);
+    
+    iquatinvert(&quat);
+    SP_TRUE(iquatisequal(&quat, &quatinvert));
+}
+
+SP_CASE(iquat, iquatdot) {
+    iquat quat;
+    __iscaler_fill_4(&quat, 1, 2, 3, 4);
+    
+    ireal dot = iquatdot(&quat, &quat);
+    SP_TRUE(ireal_equal(dot, 30));
+}
+
+SP_CASE(iquat, iquatlength) {
+    iquat quat;
+    __iscaler_fill_4(&quat, 0, 0, 3, 4);
+    
+    ireal lensqr = iquatlengthsqr(&quat);
+    SP_TRUE(ireal_equal(lensqr, 25));
+    ireal len = iquatlength(&quat);
+    SP_TRUE(ireal_equal(len, 5));
+}
+
+SP_CASE(iquat, iquatmultipy) {
+    SP_TRUE(1);
+}
+
+SP_CASE(iquat, iquatnormalize) {
+    iquat quat;
+    __iscaler_fill_4(&quat, 0, 0, 3, 4);
+    
+    iquat quatn;
+    __iscaler_fill_4(&quatn, 0, 0, 0.6, 0.8);
+    
+    iquatnormalize(&quat);
+    SP_TRUE(iquatisequal(&quat, &quatn));
+    
+    ireal len = iquatlength(&quat);
+    SP_TRUE(ireal_equal(len, 1));
+}
+
 SP_CASE(iquat, end) {
     SP_TRUE(1);
 }
