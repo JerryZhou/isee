c510b4ae41e19f13d1235af9fe7d4bdce863c7fa
jerry <jerryzhou@outlook.com>
2016-10-16 16:50:05 +0800

    radix convert fix: sign ==> 0, 1, 2: -1, 0, 1

--- code/foundation/util/iradix.c
+++ code/foundation/util/iradix.c
@@ -17,22 +17,26 @@ iarray *iradixcalc(const iarray *radixs, uint64_t value, size_t depth) {
         value = iarrayof(sizes, uint64_t, idx) % iarrayof(radixs, iradixunit, idx).radix;
         iarrayset(sizes, idx, &value);  /*idx*/
         
-        ilog("offset[%d]-size %llu, %llu\n", idx, iarrayof(sizes, uint64_t, idx), iarrayof(sizes, uint64_t, idx+1));
+        ilog("offset[%d] = %llu\n", idx, iarrayof(sizes, uint64_t, idx));
+        ilog("offset[%d] = %llu\n", idx+1, iarrayof(sizes, uint64_t, idx+1));
     } while(++idx<iarraylen(radixs) && iarrayof(sizes, uint64_t, idx) > 0 && --depth>0);
     
     /* max unit */
-    if (iarrayof(sizes, uint64_t, idx) > 0 ) {
+    value = iarrayof(sizes, uint64_t, idx);
+    if (value > 0 ) {
         value = (iarrayof(sizes, uint64_t, idx) * iarrayof(radixs, iradixunit, idx-1).radix
                  + iarrayof(sizes, uint64_t, idx-1));
         iarrayset(sizes, idx-1, &value);
+    } else if (value == 0) {
+        iarrayremove(sizes, iarraylen(sizes)-1); /* remove the last zero */
     }
-    return idx;
+    return sizes;
 }
 
 /* make a composing string */
 istring *iradixcalccompose(const iarray *radixs, const iarray* sizes) {
     /* build the size unit string */
-    int idx = iarraylen(sizes);
+    size_t idx = iarraylen(sizes);
     iarray *buf = iarraymakechar(256);
     
     /* calculating */
@@ -49,10 +53,11 @@ istring *iradixcalccompose(const iarray *radixs, const iarray* sizes) {
 }
 
 /* calcualting the radix-diff-chaning elements with sign at end of sizes
+ * sign: 0 == -1, 1 ==> 0, 2 ==> 1
  * return: iarray<uint64_t> */
 iarray *iradixdiff(const iarray *radixs, uint64_t lfs, uint64_t rfs, size_t depth) {
-    uint64_t sign = lfs > rfs ? 1 : (lfs == rfs ? 0 : -1);
-    uint64_t diff = sign > 0 ? lfs - rfs : rfs - lfs;
+    uint64_t sign = lfs > rfs ? 2 : (lfs == rfs ? 1 : 0);
+    uint64_t diff = sign > 1 ? lfs - rfs : rfs - lfs;
     iarray *sizes = iradixcalc(radixs, diff, depth);
     iarrayappend(sizes, &sign, 1); /* append the sign to ssize */
     return sizes;
@@ -61,7 +66,7 @@ iarray *iradixdiff(const iarray *radixs, uint64_t lfs, uint64_t rfs, size_t dept
 /* make a composing string with suffixs */
 istring *iradixdiffcompose(const iarray *radixs, const iarray *sizes, const iarray *suffixs) {
     /* build the size unit string */
-    size_t idx = iarraylen(sizes)-1;
+    size_t idx = iarraylen(sizes);
     iarray *buf = iarraymakechar(256);
     istring *s;
     uint64_t sign = iarrayoflast(sizes, uint64_t);
@@ -79,7 +84,7 @@ istring *iradixdiffcompose(const iarray *radixs, const iarray *sizes, const iarr
     
     /* suffix */
     if (suffixs) {
-        s = iarrayof(suffixs, istring*, sign+1);
+        s = iarrayof(suffixs, istring*, sign);
         istringrawformat(buf, "%V", s);
     }
     
