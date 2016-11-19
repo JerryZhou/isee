99ff7b830d4e4e9a565e650918a8dd997b74389f
jerry <jerryzhou@outlook.com>
2016-11-20 02:30:04 +0800

    test for ivar

--- test/foundation/test_ivar.h
+++ test/foundation/test_ivar.h
@@ -9,6 +9,76 @@ SP_CASE(ivar, ivaris) {
     irefdelete(var);
 }
 
+SP_CASE(ivar, ivartype) {
+    ivar *var = ivarmakeint(0);
+    SP_EQUAL(ivartype(var), imetaindex(int));
+    irefdelete(var);
+    ivar *nvar = irefnew(ivar);
+    SP_EQUAL(ivartype(var), imetaindex(inull));
+    irefdelete(nvar);
+}
+
+SP_CASE(ivar, ivarmeta) {
+    ivar *var = ivarmakeint(0);
+    SP_EQUAL(ivarmeta(var), imetaof(int));
+    irefdelete(var);
+}
+
+SP_CASE(ivar, ivarissimple) {
+    {
+        ivar *var = irefnew(ivar);
+        SP_TRUE(ivarissimple(var));
+        irefdelete(var);
+    }
+    {
+        ivar *var = ivarmakeint(0);
+        SP_TRUE(ivarissimple(var));
+        irefdelete(var);
+    }
+    {
+        ivar *var = ivarmakei64(0);
+        SP_TRUE(ivarissimple(var));
+        irefdelete(var);
+    }
+    {
+        ivar *var = ivarmakeu64(0);
+        SP_TRUE(ivarissimple(var));
+        irefdelete(var);
+    }
+    {
+        ivar *var = ivarmakeptr(0);
+        SP_TRUE(ivarissimple(var));
+        irefdelete(var);
+    }
+    
+    {
+        ivar *var = ivarmakereal(0);
+        SP_TRUE(ivarissimple(var));
+        irefdelete(var);
+    }
+    {
+        ivar *var = ivarmakeref(NULL);
+        SP_TRUE(ivarissimple(var));
+        irefdelete(var);
+    }
+    {
+        iref *ref = irefnew(iref);
+        ivar *var = ivarmakeref(ref);
+        SP_TRUE(!ivarissimple(var));
+        irefdelete(var);
+        irefdelete(ref);
+    }
+    {
+        ipod p;
+        p.ptr = p.stbuf;
+        
+        ivar *var = ivarmakepod(p);
+        SP_TRUE(!ivarissimple(var));
+        irefdelete(var);
+    }
+    
+}
+
 SP_CASE(ivar, end) {
     imemoryglobalclear();
     
