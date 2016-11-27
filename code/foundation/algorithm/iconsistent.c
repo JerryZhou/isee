bf3fb9ab5659560150a591f73db08ca7f507d7fe
jerry <jerryzhou@outlook.com>
2016-11-27 14:15:04 +0800

    the real implementing for consistent-algorithm

--- code/foundation/algorithm/iconsistent.c
+++ code/foundation/algorithm/iconsistent.c
@@ -1,2 +1,158 @@
 #include "foundation/algorithm/iconsistent.h"
+#include "foundation/util/idicttypes.h"
+#include "foundation/util/iarraytypes.h"
+
+
+/* the consistent destructor */
+void iconsistent_destructor(const struct imeta *meta, iptr o) {
+    iconsistent *c = icast(iconsistent, o);
+    irefdelete(c->circle);
+    irefdelete(c->elems);
+}
+
+/* make a replicas-key */
+istring *_iconsistent_key_n(iconsistent *c, istring *ele, int index) {
+    return istringformat("%S|%i", ele, index);
+}
+   
+/* add ele to c in span: replicasspan with numberofreplicas */
+void iconsistentadd(iconsistent *c, istring *ele) {
+    istring *key;
+    
+    for (int i=0; i<c->numberofreplicas; ++i) {
+        key = _iconsistent_key_n(c, ele, i);
+        idictadd_u64_istring(c->circle, iconsistenthashof(c, key), key);
+        irefdelete(key);
+    }
+    idictadd_istring_ibool(c->elems, ele, iiok);
+}
+
+/* remove ele from c in span */
+void iconsistentremove(iconsistent *c, istring *ele) {
+    istring *key;
+    
+    for (int i=0; i<c->numberofreplicas; ++i) {
+        key = _iconsistent_key_n(c, ele, i);
+        idictremove_u64(c->circle, iconsistenthashof(c, key));
+        irefdelete(key);
+    }
+    idictremove_istring(c->elems, ele);
+}
+
+int _iarray_range_search(iarray *elems, istring* key) {
+    irangearray(elems, istring*,
+                if (istringcompare(__value, key) == 0) {
+                    return iiok;
+                }
+                );
+    return iino;
+}
+
+/* set the circle to elems: remove all not in elems, add all in elems*/
+/* iarray<istring*> */
+void iconsistentset(iconsistent *c, iarray* elems) {
+    const iarray * keys = idictkeys(c->elems);
+    istring *skey;
+    
+    /* remove ele in members but not in elems */
+    irangearray(keys, idictentry*,
+                skey = ivarcast(__value->key, istring*);
+                if (!_iarray_range_search(elems, skey)) {
+                    iconsistentremove(c, skey);
+                }
+                );
+    
+    /* add elems to members */
+    irangearray(elems, istring*,
+                if (!idicthas_istring(c->elems, __value)) {
+                    iconsistentadd(c, __value);
+                }
+                );
+}
+
+/* the index of elem-key ==> hashcode */
+static uint64_t _iconsistent_key(const iconsistent *c, int index) {
+    const iarray* sortedkeys = idictkeys(c->circle);
+    idictentry *entry = iarrayof(sortedkeys, idictentry*, index);
+    return ivarcast(entry->key, uint64_t);
+}
+
+/* the index of elem-key ==> skey */
+static istring* _iconsistent_value(const iconsistent *c, int index) {
+    const iarray* sortedkeys = idictkeys(c->circle);
+    idictentry *entry = iarrayof(sortedkeys, idictentry*, index);
+    return ivarcast(entry->value, istring*);
+}
+
+/* closed ele for key */
+istring* iconsistentindexingof(const iconsistent *c, istring *key) {
+    if (idictsize(c->circle) == 0) {
+        return NULL;
+    }
+    int index = iconsistentindexing(c, key);
+    return _iconsistent_value(c, index);
+}
+
+/* closed index for key */
+int iconsistentindexing(const iconsistent *c, istring *key) {
+    ivar *keyvar = ivarmakeu64(iconsistenthashof(c, key));
+    idictentry entry = {.key=keyvar};
+    const iarray* sortedkeys = idictkeys(c->circle);
+    int index = iarraybinaryindexing(sortedkeys, 0, iarraylen(sortedkeys), &entry);
+    irefdelete(keyvar);
+    if (index >= iarraylen(sortedkeys)) {
+        index = 0;
+    }
+    return index;
+}
+
+/* closest index for key then next n key will return */
+iarray* iconsistentsearch(const iconsistent *c, istring *key, size_t n) {
+    iarray* closest = iarraymakeiref(8);
+    istring *skey;
+    size_t nc = idictsize(c->circle);
+    int start;
+    int i;
+    
+    do {
+        if (nc == 0) {
+            break;
+        }
+        n = imin(n, nc);
+        /* the first one */
+        start = iconsistentindexing(c, key);
+        skey = _iconsistent_value(c, start);
+        iarrayadd(closest, &skey);
+        if (iarraylen(closest) == n) {
+            break;
+        }
+        
+        /* the next left ones */
+        for (i = start + 1; i != start; ++i) {
+            if (i >= nc) {
+                i = 0;
+            }
+            skey = _iconsistent_value(c, start);
+            if (_iarray_range_search(closest, skey) == iino) {
+                iarrayadd(closest, &skey);
+            }
+            if (iarraylen(closest) == n) {
+                break;
+            }
+        }
+        break;
+    }while(0);
+    
+    /* no-indexing key */
+    if (iarraylen(closest)==0) {
+        irefdelete(closest);
+    }
+    return closest;
+}
+
+/* the hash-code-for key */
+uint64_t iconsistenthashof(const iconsistent *c, istring *key) {
+    return istringhashcode(key);
+}
+
 
--- code/foundation/algorithm/iconsistent.h
+++ code/foundation/algorithm/iconsistent.h
@@ -3,11 +3,56 @@
 	
 #include "foundation/itype.h"
 #include "foundation/core/iref.h"
+
+#include "foundation/util/istring.h"
 	
 /* Set up for C function definitions, even when using C++ */
 #ifdef __cplusplus
 extern "C" {
 #endif
+    
+/*
+ * https://github.com/stathat/consistent/blob/master/consistent.go
+ * https://en.wikipedia.org/wiki/Consistent_hashing */
+    
+struct idict;
+   
+/* the consistent hash-circle 
+ * add numberofreplicas of fake-hash in circle with averge of span in range
+ * */
+typedef struct iconsistent {
+    irefdeclare;
+    
+    struct idict *circle;   /* idict<uint64_t, istring*> */
+    struct idict *elems;    /* idict<istring*, ibool> */
+    
+    size_t numberofreplicas;/* number of replicas */
+    
+} iconsistent;
+
+/* the consistent destructor */
+void iconsistent_destructor(const struct imeta *meta, iptr o);
+   
+/* add ele to c in span: replicasspan with numberofreplicas */
+void iconsistentadd(iconsistent *c, istring *ele);
+
+/* remove ele from c in span */
+void iconsistentremove(iconsistent *c, istring *ele);
+
+/* set the circle to elems: remove all not in elems, add all in elems*/
+void iconsistentset(iconsistent *c, iarray* elems); /* iarray<istring*> */
+
+/* closed ele for key */
+istring* iconsistentindexingof(const iconsistent *c, istring *key);
+
+/* closed index for key */
+int iconsistentindexing(const iconsistent *c, istring *key);
+
+/* closed index for key then next n key will return */
+iarray* iconsistentsearch(const iconsistent *c, istring *key, size_t n);
+
+/* the hash-code-for key */
+uint64_t iconsistenthashof(const iconsistent *c, istring *key);
 
 /* Ends C function definitions when using C++ */
 #ifdef __cplusplus
