d88b04d79b6b815ec2e960be25d5912d97f27150
jerry <jerryzhou@outlook.com>
2016-11-27 14:38:04 +0800

    test for consistent

--- test/foundation/test_iconsistent.h
+++ test/foundation/test_iconsistent.h
@@ -2,6 +2,12 @@
 
 SP_SUIT(iconsistent);
 
+SP_CASE(iconsistent, make) {
+    iconsistent *c = iconsistentmake(20, 30);
+    SP_TRUE(iconsistentsize(c) == 0);
+    irefdelete(c);
+}
+
 SP_CASE(iconsistent, end) {
     imemoryglobalclear();
     
