389b34e005519fe4f70bc35e5bdf199ea5ee6b2d
jerry <jerryzhou@outlook.com>
2016-11-28 11:06:50 +0800

    add runtime and add gc-marksweep, gc-generation

---
+++ code/foundation/runtime/imarksweep.c
@@ -0,0 +1,2 @@
+#include "foundation/runtime/imarksweep.h"
+
---
+++ code/foundation/runtime/imarksweep.h
@@ -0,0 +1,17 @@
+#ifndef _IMARKSWEEP_H_
+#define _IMARKSWEEP_H_
+	
+#include "foundation/itype.h"
+#include "foundation/core/iref.h"
+	
+/* Set up for C function definitions, even when using C++ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Ends C function definitions when using C++ */
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _IMARKSWEEP_H_ */
