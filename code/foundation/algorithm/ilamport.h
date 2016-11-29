352682680debd5df681e61824882111c226a5516
jerry <jerryzhou@outlook.com>
2016-11-29 13:30:12 +0800

    add algorithm: ilamport todos

---
+++ code/foundation/algorithm/ilamport.h
@@ -0,0 +1,17 @@
+#ifndef _ILAMPORT_H_
+#define _ILAMPORT_H_
+	
+#include "foundation/itype.h"
+#include "foundation/core/iref.h"
+	
+/* Set up for C function definitions, even when using C++ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Ends C function definitions when using C++ */
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _ILAMPORT_H_ */
--- code/foundation/runtime/READEME.md
+++ code/foundation/runtime/READEME.md
@@ -1,3 +1,8 @@
 http://wangzhezhe.github.io/blog/2016/04/30/golang-gc/
 https://github.com/qyuhen/book
 http://newhtml.net/v8-garbage-collection/
+
+defer http://fdiv.net/2015/10/08/emulating-defer-c-clang-or-gccblocks
+
+rus cox: https://swtch.com/
+garbage-collection: https://github.com/ivmai/bdwgc.git 
