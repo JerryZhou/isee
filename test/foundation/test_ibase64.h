4f6d1c578f0c3712639852ed42b893d6cf0a5a21
jerry <jerryzhou@outlook.com>
2017-03-28 22:12:07 +0800

    base64 unit-test

--- test/foundation/test_ibase64.h
+++ test/foundation/test_ibase64.h
@@ -9,5 +9,28 @@
 #ifndef test_ibase64_h
 #define test_ibase64_h
 
+#include "foundation/util/ibase64.h"
+
+SP_SUIT(ibase64);
+
+SP_CASE(ibase64, ibase64encode) {
+    /* abc ==> YWJj */
+    istring *s = istringmake("abc");
+    istring *s0 = ibase64encode(ibase64Std(), s);
+    
+    SP_TRUE(islicelen(s0) == 4);
+    SP_TRUE(isliceof(s0, ibyte, 0) == 'Y');
+    SP_TRUE(isliceof(s0, ibyte, 1) == 'W');
+    SP_TRUE(isliceof(s0, ibyte, 2) == 'J');
+    SP_TRUE(isliceof(s0, ibyte, 3) == 'j');
+    
+    irefdelete(s);
+    irefdelete(s0);
+}
+
+SP_CASE(ibase64, end) {
+    SP_TRUE(1);
+}
+
 
 #endif /* test_ibase64_h */
