bb5b671cd6af5fffb27e28c906799465115f3922
jerry <jerryzhou@outlook.com>
2017-04-05 23:22:57 +0800

    the kv-cache dependcy on dict

--- code/foundation/util/ikvmemcache.c
+++ code/foundation/util/ikvmemcache.c
@@ -29,18 +29,107 @@ void ikvmemcacheclear(ikvmemcache* cache) {
 size_t ikvmemcachetrim(ikvmemcache* cache, size_t trim) {
     icheckret(ireflistlen(cache->lru) > trim, 0);
     
-    /*TODO: double-linked-list in circle */
+    /*double-linked-list in circle */
+    irefjoint* node = ireflistlast(cache->lru);
+    irefjoint* pre = NULL;
+    int rangelimit = idictsize(cache->dict);
+    idictentry* entry = NULL;
+    
+    /* the trimed-cache */
+    idict* toremoved = NULL;
+    if (cache->envicted) {
+        toremoved = idictmake(8);
+    }
+    
+    /* reverse the lru-list */
+    while (idictsize(cache->dict) > trim && rangelimit > 0) {
+        if (node) {
+            pre = node->pre;
+            if (!iflag_is(cache->flag, EnumKvMemCacheFlag_NotTrimTheRetained) || // no need to track the retain
+                (entry = icast(idictentry, node->value) &&                       // track the retain-count is less equal than 1
+                 (entry == NULL || entry->value == NULL || entry->value->_ref == 1))) {
+                    /* remove from the lru */
+                    ireflistremovejoint(cache->lru, node);
+                    /* remove from the dict */
+                    if (entry && entry->key) {
+                        /* the call-back datas */
+                        if (toremoved) {
+                            idictadd(toremoved, entry->key, entry->value);
+                        }
+                        idictremoveentry(cache->dict, entry, NULL);
+                    }
+            }
+            /* pre-node */
+            node = pre;
+        } else {
+            break;
+        }
+        --rangelimit;
+    }
+    
+    /* the triming call-back */
+    if (toremoved) {
+        cache->envicted(cache, toremoved);
+        irefdelete(toremoved);
+    }
     
     return 0;
 }
 
+/* update the joint in lru */
+static void _ikvmemcacheupdateentry(ikvmemcache* cache, idictentry *entry) {
+    irefjoint* joint = icast(irefjoint, entry->u);
+    iretain(joint);
+    ireflistremovejoint(cache->lru, joint);
+    ireflistaddjoint(cache->lru, joint);
+    irelease(joint);
+}
+
 /* get the value of cache, not need to release for ivar */
 struct ivar * ikvmemcacheget(const ikvmemcache* cache, const struct ivar* key) {
+    idictentry* entry = idictentryof(cache->dict, key);
+    if (entry) {
+        _ikvmemcacheupdateentry((ikvmemcache*)cache, entry);
+        return entry->value;
+    }
     return NULL;
 }
 
 /* put the ivar in key */
-void ikvmemcacheput(struct ikvmemcache* cache, const struct ivar *key, struct ivar *value) {
+int ikvmemcacheput(struct ikvmemcache* cache, const struct ivar *key, struct ivar *value) {
+    idictentry* entry = idictentryof(cache->dict, key);
+    int exits = iino;
+    if (!entry) {
+        entry = idictadd(cache->dict, key, value);
+        entry->u = ireflistadd(cache->lru, irefcast(entry));
+    } else {
+        exits = iiok;
+        idictentysetvalue(entry, value);
+        
+        /* update the joint in lru */
+        _ikvmemcacheupdateentry((ikvmemcache*)cache, entry);
+    }
     
+    /* if add object and the flag is auto-triming */
+    if (!exits && iflag_is(cache->flag, EnumKvMemCacheFlag_AutoTrimToCapacity)) {
+        ikvmemcachetrim(cache, cache->capacity);
+    }
+    
+    return exits;
+}
+
+/* remove the cache for key */
+int ikvmemcacheremove(ikvmemcache* cache, const struct ivar *key) {
+    idictentry* entry = idictentryof(cache->dict, key);
+    if (entry) {
+        /* remove from the lru */
+        irefjoint* joint = icast(irefjoint, entry->u);
+        ireflistremovejoint(cache->lru, joint);
+        
+        /* remove from the dict */
+        idictremoveentry(cache->dict, entry, NULL);
+    }
+    
+    return entry != NULL;
 }
 
--- code/foundation/util/ikvmemcache.h
+++ code/foundation/util/ikvmemcache.h
@@ -14,10 +14,16 @@ extern "C" {
     struct idict;
     struct ireflist;
     struct ivar;
+    struct ikvmemcache;
+    
+    /* forwrad the envicted-block */
+    typedef void (*ikvcache_envicted)(struct ikvmemcache* cache, struct idict* values);
     
     /* the control flag in kv-cache */
     typedef enum EnumKvMemCacheFlag {
         EnumKvMemCacheFlag_NotTrimTheRetained = 1,      /* do not trime the retained object */
+        
+        EnumKvMemCacheFlag_AutoTrimToCapacity = 1<<1,   /* auto trim the cache when add object */
         
     } EnumKvMemCacheFlag;
     
@@ -29,6 +35,11 @@ extern "C" {
         
         struct idict *dict;             /* the key-value */
         struct ireflist *lru;           /* the lru node list */
+        
+        ikvcache_envicted envicted;     /* the call-back */
+        
+        size_t capacity;                /* the max-count of cache-object */
+        
     } ikvmemcache;
     
     /* make a cache */
@@ -46,8 +57,11 @@ extern "C" {
     /* get the value of cache, not need to release for ivar */
     struct ivar * ikvmemcacheget(const ikvmemcache* cache, const struct ivar* key);
     
-    /* put the ivar in key */
-    void ikvmemcacheput(struct ikvmemcache* cache, const struct ivar *key, struct ivar *value);
+    /* put the ivar in key, if the new key return iiok, if exits already return iino */
+    int ikvmemcacheput(struct ikvmemcache* cache, const struct ivar *key, struct ivar *value);
+    
+    /* remove the cache for key */
+    int ikvmemcacheremove(ikvmemcache* cache, const struct ivar *key);
 
 /* Ends C function definitions when using C++ */
 #ifdef __cplusplus
